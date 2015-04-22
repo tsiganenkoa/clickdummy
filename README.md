950358c1bcf4c72b9da595441f0b6a85e8aa4367
Wang Kang <wangkang89@hotmail.com>
2015-04-22 22:24:28 +0800

    initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# clickdummy
-YAAYorNAAY.com
---
+++ www/js/constants.js
@@ -0,0 +1,7 @@
+'use strict';
+
+angular.module('myApp.constants', [])
+        .constant('AppConfig', {
+          appName: 'YAAYorNAAY',
+          endpoint: 'https://example.domain.com/api2/v1/'
+        });
