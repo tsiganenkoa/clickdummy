e0d9995b488cec373d1216089898ad99a7522052
Wang Kang <wangkang89@hotmail.com>
2015-04-24 02:23:47 +0800

    categories page

--- www/js/controllers/Categories.js
+++ www/js/controllers/Categories.js
@@ -2,4 +2,49 @@ angular.module('myApp.controllers').controller('CategoriesCtrl', function ($scop
   $scope.loginWithEmail = function () {
     $location.path('/login');
   };
+  
+  $scope.categories = [
+    {
+      id: 1,
+      image: 'img/examples/1.jpg',
+      cup: 'gold',
+      category: 'pink',
+      name: 'Most Beautiful Pic of the week'
+    },
+    {
+      id: 2,
+      image: 'img/examples/2.jpg',
+      cup: 'silver',
+      category: 'blue',
+      name: 'Hello Dolley?'
+    },
+    {
+      id: 3,
+      image: 'img/examples/3.jpg',
+      cup: 'bronze',
+      category: 'blue',
+      name: 'Stylish Pic?'
+    },
+    {
+      id: 4,
+      image: 'img/examples/4.jpg',
+      cup: 'gold',
+      category: 'grey',
+      name: 'Most Beautiful Pic of the week'
+    },
+    {
+      id: 5,
+      image: 'img/examples/5.jpg',
+      cup: 'gold',
+      category: 'blue',
+      name: 'Most Beautiful Pic of the week'
+    },
+    {
+      id: 6,
+      image: 'img/examples/6.jpg',
+      cup: 'bronze',
+      category: 'grey',
+      name: 'Stylish Pic?'
+    }
+  ];
 });
